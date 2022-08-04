a2b8a51820bf7f2371d783c5a4c81c2fb29bfd23
laurentperroteau <kawtarrouizi182@gmail.com>
2022-08-04 03:46:32 -0700

    first commit

---
+++ projet.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    float n1,n2,res;
+    char OP;
+    printf("entrer votre premier nombre: ");
+    scanf(" %f",&n1);
+    printf("entrer la desieme nombre: ");
+    scanf(" %f",&n2);
+    printf("donner un OP: ");
+    scanf(" %C",&OP);
+
+    switch (OP){
+           case '+': res = n1+n2;
+           break;
+           case '-': res = n1-n2;
+           break;
+           case '*': res = n1*n2;
+           break;
+           case '/': res = n1/n2;
+           break;
+           case 'q':
+                printf("le programme est quiter");
+                break;
+          default: printf("cette operation n'exist pas");
+          break;
+
+           }
+    printf(" le resultat = %f",res);
+     return 0;
+
+}
